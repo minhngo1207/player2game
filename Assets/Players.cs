e137873f29f13ac88240c47a5e9f0abeb6294c67
minhngo1207 <1023252@student.sae.edu.au>
2022-06-01 15:30:48 +1000

    pong

---
+++ Assets/Players.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Players : MonoBehaviour
+{
+    public bool isPlayer1;
+    public float speed;
+    public Rigidbody2D rb;
+
+    private float movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayer1)
+        {
+            movement = Input.GetAxisRaw("Vertical");
+        }
+        else 
+        {
+            movement = Input.GetAxisRaw("Vertical1");
+        }
+        rb.velocity = new Vector2(rb.velocity.x, movement * speed);
+    }
+}
